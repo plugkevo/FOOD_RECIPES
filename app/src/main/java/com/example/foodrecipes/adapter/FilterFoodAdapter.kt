aa660ab299fb5a53f7a7712c7345ee9b5b0f065d
plugkevo <munenekevin366@gmail.com>
2023-07-18 14:16:38 +0300

    Ui almost done

---
+++ app/src/main/java/com/example/foodrecipes/adapter/FilterFoodAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.foodrecipes.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.foodrecipes.R
+import com.example.foodrecipes.model.ModelFilter
+
+class FilterFoodAdapter(private val mContext: Context, private val items: List<ModelFilter>, private val onSelectData: onSelectData) :
+    RecyclerView.Adapter<FilterFoodAdapter.ViewHolder>() {
+
+    interface onSelectData {
+        fun onSelected(modelMain: ModelFilter)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_item_filter_food, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = items[position]
+
+        //Get Image
+        Glide.with(mContext)
+            .load(data.strMealThumb)
+            .placeholder(R.drawable.ic_food_placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(holder.imgThumb)
+
+        holder.tvMeal.text = data.strMeal
+        holder.cvFilterMeal.setOnClickListener { onSelectData.onSelected(data) }
+        holder.imgFavorite.setOnClickListener {
+            Toast.makeText(mContext, "Feature under development", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    //Class Holder
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvMeal: TextView = itemView.findViewById(R.id.tvMeal)
+        val cvFilterMeal: CardView = itemView.findViewById(R.id.cvFilterMeal)
+        val imgThumb: ImageView = itemView.findViewById(R.id.imgThumb)
+        val imgFavorite: ImageView = itemView.findViewById(R.id.imgFavorite)
+    }
+}
---
+++ app/src/main/java/com/example/foodrecipes/adapter/MainAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.foodrecipes.adapter
+
+class MainAdapter {
+}
